f569bef374d69257c00a4ba41e83cb780164db11
Alexis Kinsella <alexis.kinsella@gmail.com>
2016-12-27 01:08:55 +0100

    Fixed tests

--- test/stationFetcherTest.js
+++ test/stationFetcherTest.js
@@ -3,14 +3,34 @@ const sinon = require('sinon');
 const chai = require('chai');
 const fs = require('fs');
 const expect = require('chai').expect;
+const Promise = require('bluebird');
+const _ = require('lodash');
 
 const stationFetcher = require('../lib/stationFetcher');
+const stationImageFetcher = require('../lib/stationImageFetcher');
 
 describe('Station Fetcher', () => {
 
     const stations = JSON.parse(fs.readFileSync('./data/stations.json'));
 
     before((done) => {
+
+        sinon
+            .stub(stationImageFetcher, 'fetch')
+            .returns(Promise.resolve([{
+                number: 44101,
+                width: 640,
+                quality: 60,
+                contractName: 'Paris',
+                uid: 1
+            }, {
+                number: 44101,
+                width: 640,
+                quality: 60,
+                contractName: 'Paris',
+                uid: 2
+            }]));
+
         sinon
             .stub(request, 'get')
             .yields(null, { statusCode: 200 }, stations);
@@ -18,17 +38,24 @@ describe('Station Fetcher', () => {
     });
 
     after(function(done){
+        stationImageFetcher.fetch.restore();
         request.get.restore();
         done();
     });
 
     it('Should Fetch stations', (done) => {
 
-        const city = 'Paris';
+        const contractName = 'Paris';
         const apiKey = process.env.API_KEY;
 
-        stationFetcher.fetch(city, apiKey).then((stations) => {
+        stationFetcher.fetch(contractName, apiKey).then((stations) => {
+
+            const station = _.find(stations, station => station.number === 44101);
+
+            console.log('station:', station);
+
             expect(stations.length).to.equal(1218);
+            expect(station.images.length).to.equal(2);
             done();
         }).catch((err) => {
             done(err);
